da8ad68bc4723970b89fcf78bf6289cf92f9cecc
htoomyat-pc <htoomyat-pc@localhost>
2021-03-15 18:19:28 +0630

    a

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const Message = require("../src/Message");
 const storage = [];
 
+let lastUpdated;
+
 router.get('/', (req, res) => {
     const verified = req.query["hub.verify_token"];
     const challenge = req.query["hub.challenge"];
@@ -9,17 +11,31 @@ router.get('/', (req, res) => {
     if (process.env.APP_SECRET === verified) {
         res.send(challenge).end();
     } else {
-        res.status(403).end();
+        if (req.session['loggedIn']) {
+            res.json({
+                count: storage.total,
+                messages: storage,
+                lastUpdated: new Date(lastUpdated),
+            });
+        } else {
+            res.status(403).end();
+        }
     }
 });
 
 router.post('/', (req, res) => {
     const verified = req.query["hub.verify_token"];
     if (process.env.APP_SECRET === verified) {
+        lastUpdated = Date.now();
         // Message.create({
         //   message: '',
         // });
-
+        console.log(req.query, req.body);
+        storage.push({
+            q: req.query,
+            d: req.body,
+        })
+        res.status(200).end();
     } else {
         res.status(403).end();
     }
