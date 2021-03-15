78d70d7c1d7d51bfd608d7b50b2be98e9124ef20
Shin Maung <ethereal97@gmail.com>
2021-03-15 06:44:00 +0630

    f

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,10 +4,12 @@ const storage = require("../src/storage");
 router.get('/', (req, res) => {
   const verified = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
-  if (Object.values(req.body).length) {
-    storage.push(req.body);
-  }
   res.send(challenge).end();
+});
+
+router.post('/', (req, res) => {
+  storage.push(req.body);
+  res.end();
 });
 
 module.exports = router;
